1e830998a36ad4a3da065163f52bc6d56363d8ba
Diego Ojeda Rivera <diegojeda775@yahoo.com>
2021-02-18 19:14:41 -0800

    added instructions and about section

--- src/about/about.js
+++ src/about/about.js
@@ -3,8 +3,25 @@ import React, { Component } from 'react'
 export default class About extends Component {
     render() {
         return (
-            <div>
-                Why I created the app.
+            <div className='about'>
+                <h4>About</h4>
+
+                <h5>Why build this app?</h5>
+
+                <h6>
+                    I built this app because my wife and her friends are artists and they always strive to get the best quality production for projects or auditions.
+                    Since meeting my wife, I have learned many ways set up for self-tapping (online auditioning). We have acquired equipment over the years albeit not expensive, but the value for the money.
+                    Therefore I sought to create a place where we can keep track of the equipment/gear we have as well as the important information in case of emergencies.
+                </h6>
+                <h6>
+                    Well, I didn't just built the app for my wife's equipment, but also to keep track of any gadgets that I would acquire like videogame cosoles or headphones.
+                    Even thought I like collecting videogames or trading card games, this app will be useful for any kind products like appliances, phone accesories and more. 
+                    I hope that this app will be useful for other people too. 
+                </h6>
+                <h6>
+                    Thank you for visiting!
+                </h6>
+                    
             </div>
         )
     }
--- src/instructions/instructions.js
+++ src/instructions/instructions.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Instructions extends Component {
     render() {
         return (
             <div>
-                This is how you use the app.
+                 <h4>Wellcome to Gear Vault</h4>
+
+                 <h6>
+                    This is an web-app where you can keep information of your equipment/gear for tracking or insurance purposes!
+                </h6>
+
+                <h5>Instructions</h5>
+
+                <h6>
+                    In this web-app you'll be able to input items to a list where you can add the name, brand, price, serial number, date of purchase and place of purchase.
+                    In addition, you will be able to remove and/or update existing items. There are few examples in the list which you can delete and add your own specifi items.
+                    If you would like to give it a try, please got to the "list" tab on the navigation menu or click the button below!
+                </h6>
+                <h6>
+                    If you would like to know the inspiration behind this web-app, go to the "About" section on the navigation menu.
+                </h6>
+                <h6>
+                    Thank you for visiting!
+                </h6>
+
+                <Link to='/list'>
+                    <button>Go to List</button>
+                </Link>
+
             </div>
         )
     }
